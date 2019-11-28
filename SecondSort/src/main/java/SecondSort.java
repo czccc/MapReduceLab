38910ae49536afee73020a824db58ffc73098ab7
Cheng <lichchchn@gmail.com>
2019-11-28 16:33:16 +0800

    update SecondSort

--- SecondSort/src/main/java/SecondSort.java
+++ SecondSort/src/main/java/SecondSort.java
@@ -1,24 +1,14 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.FileSplit;
-
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-
-import java.io.DataInput;
-import java.io.DataOutput;
 import java.io.IOException;
-
-import java.util.Collections;
-import java.util.Vector;
 
 
 
